6604746dcea89004d4c91672aded76640f79606d
Robert Gessler <RAGessler@gmail.com>
2022-07-20 13:41:20 -0500

    chore: Made some slight text edits to home page

--- hacker-news-search/src/pages/HomePage/HomePage.jsx
+++ hacker-news-search/src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,9 @@ const HomePage = ()=>{
         <div>
             <h1>Hi there 👋</h1>
             <p>My name is Robert, thanks for giving me a look!  I am excited to hear your feedback on this little app and meet more of the team!
+
+                Please feel free to check out more of my github repos for my projects.  I espicially reccomend my personalCRM that I built as a capstone project.
+                You can also check it out with a live demo here: http://crmfrontend.us-east-1.elasticbeanstalk.com/
             </p>
             
         </div>
